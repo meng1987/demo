5a96bc5e0c4f51cabe0cd32cd42165502052cdce
meng1987 <15128888718@139.com>
2018-08-02 11:14:53 +0800

    add

---
+++ resources/views/reception/admin_show.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>会员登录界面模板</title>
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/body.css') }}"/> 
+</head>
+<body>
+<div class="container">
+	<section id="content">
+		<form action="">
+			<h1>会员登录</h1>
+			<div>
+				<input type="text" placeholder="邮箱" required="" id="username" />
+			</div>
+			<div>
+				<input type="password" placeholder="密码" required="" id="password" />
+			</div>
+			 <div class="">
+				<span class="help-block u-errormessage" id="js-server-helpinfo">&nbsp;</span>			</div> 
+			<div>
+				<!-- <input type="submit" value="Log in" /> -->
+				<input type="submit" value="登录" class="btn btn-primary" id="js-btn-login"/>
+				<a href="#">忘记密码?</a>
+				<!-- <a href="#">Register</a> -->
+			</div>
+		</form><!-- form -->
+		 <div class="button">
+			<span class="help-block u-errormessage" id="js-server-helpinfo">&nbsp;</span>
+		</div> <!-- button -->
+	</section><!-- content -->
+</div>
+<!-- container -->
+
+
+<br><br><br><br>
+<div style="text-align:center;">
+<p>来源:More Templates <a href="#" target="_blank" title="涛哥文化传媒有限公司">涛哥文化传媒有限公司</a> - Collect from <a href="#" title="网页模板" target="_blank">网页模板</a></p>
+</div>
+</body>
+</html>
